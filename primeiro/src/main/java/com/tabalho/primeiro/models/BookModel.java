fa4e438c30b86e053ce0208f4a284f066f09f590
Vitor Goto <vitorgoto66@gmail.com>
2025-03-05 22:10:22 -0300

    up

---
+++ primeiro/src/main/java/com/tabalho/primeiro/models/BookModel.java
@@ -0,0 +1,31 @@
+package com.tabalho.primeiro.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class BookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titulo;
+    private String autor;
+
+    public BookModel() {}
+
+    public BookModel(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+}
+
